5d0042a144d1b6a515233cf3e0fca7f2db4ce289
drofoxbrandings <85907677+drofoxbrandings@users.noreply.github.com>
2021-10-10 14:01:28 +0400

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -55,7 +55,7 @@ const Footer = () => {
               </Typography>
               <div className={classes.footerSocialIcons}><SocialIcons /></div>
               <div className={classes.gmap}>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3611.1856652670094!2d55.37987231500881!3d25.163207983911978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0!2zMjXCsDA5JzQ3LjYiTiA1NcKwMjInNTUuNCJF!5e0!3m2!1sen!2sae!4v1633538765721!5m2!1sen!2sae"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3611.1856652670094!2d55.37987231500881!3d25.163207983911978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjXCsDA5JzQ3LjYiTiA1NcKwMjInNTUuNCJF!5e0!3m2!1sen!2sae!4v1633859958194!5m2!1sen!2sae"></iframe>
               </div>
             </Box>
           </Grid>
