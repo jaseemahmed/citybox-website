d18367d118950b9c62f5d5a5bb141cb97239bed6
fazaaITRepo <git@fazaa.ae>
2023-01-17 21:35:14 +0400

    addition gtm module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+    gtmId: 'AW-10794988153'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.render(<App />, document.getElementById("root"));
